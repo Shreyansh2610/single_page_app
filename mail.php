71db5d01058589a0141501cb5cbaec26b67a5f77
Shreyansh2610 <shreyansh7874056628@gmail.com>
2024-02-20 23:02:03 +0530

    theme changed

--- mail.php
+++ mail.php
@@ -4,14 +4,13 @@
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Get the form fields and remove whitespace.
         $name = strip_tags(trim($_POST["name"]));
-        $name = str_replace(array("\r","\n"),array(" "," "),$name);
+            $name = str_replace(array("\r","\n"),array(" "," "),$name);
         $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-        $subject = trim($_POST["subject"]);
-        $number = trim($_POST["number"]);
+        $subject = strip_tags(trim($_POST["subject"]));
         $message = trim($_POST["message"]);
 
         // Check that data was sent to the mailer.
-        if ( empty($name) OR empty($message) OR empty($number) OR empty($subject) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        if ( empty($name) OR empty($subject) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
             // Set a 400 (bad request) response code and exit.
             http_response_code(400);
             echo "Please complete the form and try again.";
@@ -20,15 +19,15 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "surance@gmail.com";
+        $recipient = "info@yourmail.com";
 
         // Set the email subject.
-        $subject = "New contact from $subject";
+        $subject = "New contact from $name";
 
         // Build the email content.
         $email_content = "Name: $name\n";
-        $email_content .= "Subject: $subject\n";
         $email_content .= "Email: $email\n\n";
+        $email_content .= "Subject: $subject\n\n";
         $email_content .= "Message:\n$message\n";
 
         // Build the email headers.
