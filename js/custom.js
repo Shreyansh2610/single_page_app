54f0c3a7c11fa0e2077992c305c8a7e9bcbff05f
Shreyansh2610 <shreyansh7874056628@gmail.com>
2024-02-24 14:40:01 +0530

    issue fixed

--- js/custom.js
+++ js/custom.js
@@ -5,3 +5,34 @@ $('.imageChange').on('change',function(event) {
     $('.changeable-background-image').attr('src',$(this).val());
     }, 50);
 });
+
+
+$('.moving-card-blocks').owlCarousel({
+    items:4,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:100,
+    autoplayHoverPause:true,
+    navText: [ '<span class="icofont-thin-left"></span>', '<span class="icofont-thin-right"></span>' ],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        800:{
+            items:2
+        },
+        1024:{
+            items:2
+        },
+        1200:{
+            items:3
+        },
+        1500:{
+            items:4
+        }
+    }
+});
