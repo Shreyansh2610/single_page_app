4319af68d58d5158fac2195ac6bd87436a3bbfd0
Shreyansh2610 <shreyansh7874056628@gmail.com>
2024-02-24 12:24:38 +0530

    click photo change done

--- js/custom.js
+++ js/custom.js
@@ -1,28 +1,7 @@
-// const marquee = document.querySelector('.marquee');
-// const content = marquee.querySelector('.card');
-
-// // Adjust these values as needed
-// const speed = 700; // Scrolling speed in milliseconds
-// const direction = 'left'; // 'left' or 'right'
-
-// let contentWidth = content.offsetWidth;
-// let scrollPosition = 0;
-
-// function scrollText() {
-//   if (direction === 'left') {
-//     scrollPosition -= speed;
-//     if (scrollPosition < -contentWidth) {
-//       scrollPosition = marquee.clientWidth;
-//     }
-//   } else {
-//     scrollPosition += speed;
-//     if (scrollPosition > marquee.clientWidth) {
-//       scrollPosition = -contentWidth;
-//     }
-//   }
-
-//   marquee.style.transform = `translateX(${scrollPosition}px)`;
-//   requestAnimationFrame(scrollText);
-// }
-
-// requestAnimationFrame(scrollText);
+$('.imageChange').on('change',function(event) {
+    $('.imageChange').not($(this)).filter('[checked]').removeAttr('checked');
+    setTimeout(() => {
+        $(this).attr('checked',true);
+    $('.changeable-background-image').attr('src',$(this).val());
+    }, 50);
+});
